7ff67c91ecd095a5b6555ce78ae5acaeb781fdfc
Ayush Mukhopadhyay <ayushmukho@gmail.com>
2022-12-10 21:08:32 +0530

    Created controllers and routes for post --backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+//Using middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Importing routes
+const postRoute = require("./routes/postRoute");
+
+//Using Routes
+app.use("/api/v1", postRoute);
 
 module.exports = app;
