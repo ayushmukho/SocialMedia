1af7619b524f16d5332c78e6c16fb1e4e1e8d578
Ayush Mukhopadhyay <ayushmukho@gmail.com>
2023-01-13 15:06:32 +0530

    Like and Unlike post API --backend

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createPost } = require("../controllers/postController");
+const { createPost, likeAndUnlikePost } = require("../controllers/postController");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
+router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost);
 
 module.exports = router;
