dbabebf51d177fd6270c3a263ed4f1bb49903bd5
Ayush Mukhopadhyay <ayushmukho@gmail.com>
2023-01-14 20:25:34 +0530

    Set up redux-t0olkit and

---
+++ frontend/src/Components/Login/Login.jsx
@@ -0,0 +1,55 @@
+import { Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  //handlers
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    //dispatch(loginUser(email, password));
+  }
+  return (
+    <div className="login">
+      <form className="loginForm" onSubmit={loginHandler}>
+        <Typography variant="h3" style={{ padding: "2vmax" }}>
+          Social Media
+        </Typography>
+
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Link to="/forgot/password">
+          <Typography>Forgot Password?</Typography>
+        </Link>
+
+        <Button type="submit">Login</Button>
+
+        <Link to="/register">
+          <Typography>New User?</Typography>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/Redux/Reducers/userReducer.js
@@ -0,0 +1,58 @@
+import { createReducer, createAction } from "@reduxjs/toolkit";
+
+const LoginRequest = createAction("LoginRequest");
+const LoginSuccess = createAction("LoginSuccess");
+const LoginFailure = createAction("LoginFailure");
+
+const RegisterRequest = createAction("RegisterRequest");
+const RegisterSuccess = createAction("RegisterSuccess");
+const RegisterFailure = createAction("RegisterFailure");
+
+const LoadUserRequest = createAction("LoadUserRequest");
+const LoadUserSuccess = createAction("LoadUserSuccess");
+const LoadUserFailure = createAction("LoadUserFailure");
+
+
+const initialState = {};
+
+export const userReducer = createReducer(initialState, (builder) => {
+  //Login User
+  builder
+  .addCase(LoginRequest, (state) => {
+    state.loading = true
+  })
+  .addCase(LoginSuccess, (state, action) => {
+    state.loading = false;
+    state.user = action.payload
+  })
+  .addCase(LoginFailure, (state, action) => {
+    state.loading = false;
+    state.error = action.payload
+  })
+
+  //Register User
+  .addCase(RegisterRequest, (state) => {
+    state.loading = true
+  })
+  .addCase(RegisterSuccess, (state, action) => {
+    state.loading = false;
+    state.user = action.payload
+  })
+  .addCase(RegisterFailure, (state, action) => {
+    state.loading = false;
+    state.error = action.payload
+  })
+
+  //Load User
+  .addCase(LoadUserRequest, (state) => {
+    state.loading = true
+  })
+  .addCase(LoadUserSuccess, (state, action) => {
+    state.loading = false
+    state.user = action.payload
+  })
+  .addCase(LoadUserFailure, (state, action) => {
+    state.loading = false;
+    state.error = action.payload
+  })
+});
